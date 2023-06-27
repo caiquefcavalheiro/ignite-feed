ae88c70b960125e5781eac287ddc8447d6badccc
Caique F. Cavalheiro <caiquefcavalheiro@gmail.com>
2023-06-27 20:00:43 -0300

    feat: creating post data structure and iterating over

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,45 @@ import { Post } from "./components/Post";
 
 import styles from "./App.module.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/caiquefcavalheiro.png",
+      name: "Caique Cavalheiro",
+      role: "full stack developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2023-06-26 21:30:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/caiquefcavalheiro.png",
+      name: "Caique Cavalheiro",
+      role: "full stack developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2023-06-27 23:30:00"),
+  },
+];
+
 function App() {
   return (
     <div>
@@ -11,12 +50,16 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar></Sidebar>
         <main>
-          <Post
-            author="Caique Cavalheiro"
-            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime nisi voluptatum vero distinctio, quidem magni at voluptatibus. Sunt, soluta expedita, doloremque quo ex odit asperiores at quidem minima odio illo!"></Post>
-          <Post
-            author="Gabriel Buzzi"
-            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime nisi voluptatum vero distinctio, quidem magni at voluptatibus. Sunt, soluta expedita, doloremque quo ex odit asperiores at quidem minima odio illo!"></Post>
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
